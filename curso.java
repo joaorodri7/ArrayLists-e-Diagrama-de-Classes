3f651c5a1f9fcae731236628fcb7326a6b9e7943
joaorodri7 <107088457+joaorodri7@users.noreply.github.com>
2023-06-05 13:55:54 -0300

    Create curso.java

---
+++ curso.java
@@ -0,0 +1,17 @@
+package com.mycompany.pessoa;
+
+public class Curso {
+    private String nome;
+
+    public Curso(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
