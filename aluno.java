0be248f483647d45a9a12085a94fa3360c43ed3b
joaorodri7 <107088457+joaorodri7@users.noreply.github.com>
2023-06-05 13:56:28 -0300

    Create aluno.java

---
+++ aluno.java
@@ -0,0 +1,28 @@
+package com.mycompany.pessoa;
+
+public class Aluno {
+    private String nome;
+    private Curso curso;
+
+    public Aluno(String nome, Curso curso) {
+        this.nome = nome;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
+
